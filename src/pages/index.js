d1241dc2808d7dc615387d27c080005bc112f658
Kris Chase <kris@mehh.org>
2019-12-18 22:46:38 -0800

    Fixed video component

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export default props => (
           <hr />
           <iframe id="ytplayer" type="text/html" width="720" height="405"
           src="https://www.youtube.com/embed/M7lc1UVf-VE"
-          frameborder="0" allowfullscreen>
+          frameborder="0" allowfullscreen></iframe>
         </section>
       </article>
     </Reveal>
